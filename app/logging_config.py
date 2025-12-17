4379cefdf1fa645836e3ff451fcfe6399058b89c
Serah El Rashidi <serah.elrashidi@gmail.com>
2025-12-17 11:57:08 +0200

    chore: add basic logging configuration

---
+++ app/logging_config.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import logging
+from logging.config import dictConfig
+
+
+def setup_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "default": {
+                    "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                }
+            },
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": "default",
+                }
+            },
+            "root": {
+                "handlers": ["console"],
+                "level": "INFO",
+            },
+        }
+    )
+
+
+setup_logging()
